a0593b6c1cdee1bfbe040257bb099aa80b27ef30
Joel Torstensson <oed3933@gmail.com>
2018-05-17 14:02:43 +0200

    More modularization

--- contracts/BondingCurvedToken.sol
+++ contracts/BondingCurvedToken.sol
@@ -1,18 +1,49 @@
 
 import "tokens/eip20/EIP20.sol";
+import "zeppelin/contracts/math/SafeMath.sol";
 
 
 contract BondingCurvedToken is EIP20 {
 
+    using SafeMath for uint256;
+
     uint256 public poolBalance;
+
+    constructor(
+        string name,
+        uint8 decimals,
+        string symbol
+    ) EIP20(0, name, decimals, symbol) public {}
 
     function priceToMint(uint256 numTokens) public returns(uint256);
 
     function rewardForBurn(uint256 numTokens) public returns(uint256);
 
-    function mint(uint256 numTokens) public payable;
+    function mint(uint256 numTokens) public payable {
+        uint256 priceForTokens = priceToMint(numTokens);
+        require(msg.value >= priceForTokens);
 
-    function burn(uint256 numTokens) public;
+        totalSupply = totalSupply.add(numTokens);
+        balances[msg.sender] = balances[msg.sender].add(numTokens);
+        poolBalance = poolBalance.add(msg.value);
+        if (msg.value > priceForTokens) {
+            msg.sender.transfer(msg.value - priceForTokens);
+        }
+
+        emit Minted(numTokens, priceForTokens);
+    }
+
+    function burn(uint256 numTokens) public {
+        require(balances[msg.sender] >= numTokens);
+
+        uint256 ethToReturn = rewardForBurn(numTokens);
+        totalSupply = totalSupply.sub(numTokens);
+        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+        poolBalance = poolBalance.sub(ethToReturn);
+        msg.sender.transfer(ethToReturn);
+
+        emit Burned(numTokens, ethToReturn);
+    }
 
     event Minted(uint256 amount, uint256 totalCost);
     event Burned(uint256 amount, uint256 reward);
--- contracts/PolynomialCurvedToken.sol
+++ contracts/PolynomialCurvedToken.sol
@@ -1,16 +1,12 @@
 /*pragma solidity ^0.4.23*/
 
-import "zeppelin/contracts/math/SafeMath.sol";
 import "./BondingCurvedToken.sol";
 
 
 contract PolynomialCurvedToken is BondingCurvedToken {
 
-    using SafeMath for uint256;
-
     uint256 constant private PRECISION = 10000000000;
 
-    uint256 public poolBalance;
     uint8 public exponent;
 
     constructor(
@@ -18,7 +14,7 @@ contract PolynomialCurvedToken is BondingCurvedToken {
         uint8 decimals,
         string symbol,
         uint8 _exponent
-    ) EIP20(0, name, decimals, symbol) public {
+    ) BondingCurvedToken(name, decimals, symbol) public {
         exponent = _exponent;
     }
 
@@ -35,33 +31,4 @@ contract PolynomialCurvedToken is BondingCurvedToken {
     function rewardForBurn(uint256 numTokens) public returns(uint256) {
         return poolBalance - curveIntegral(totalSupply - numTokens);
     }
-
-    function mint(uint256 numTokens) public payable {
-        uint256 priceForTokens = priceToMint(numTokens);
-        require(msg.value >= priceForTokens);
-
-        totalSupply = totalSupply.add(numTokens);
-        balances[msg.sender] = balances[msg.sender].add(numTokens);
-        poolBalance = poolBalance.add(msg.value);
-        if (msg.value > priceForTokens) {
-            msg.sender.transfer(msg.value - priceForTokens);
-        }
-
-        emit Minted(numTokens, priceForTokens);
-    }
-
-    function burn(uint256 numTokens) public {
-        require(balances[msg.sender] >= numTokens);
-
-        uint256 ethToReturn = rewardForBurn(numTokens);
-        totalSupply = totalSupply.sub(numTokens);
-        balances[msg.sender] = balances[msg.sender].sub(numTokens);
-        poolBalance = poolBalance.sub(ethToReturn);
-        msg.sender.transfer(ethToReturn);
-
-        emit Burned(numTokens, ethToReturn);
-    }
-
-    event Minted(uint256 amount, uint256 totalCost);
-    event Burned(uint256 amount, uint256 reward);
 }
